a2d557157ac25cff1bf3b384556dc1e4a0d51571
Richar2 <richard.coimec91@hotmail.com>
2019-03-20 18:08:15 -0300

    atualizacao

--- resources/views/componets/cliente.blade.php
+++ resources/views/componets/cliente.blade.php
@@ -1,1 +1,122 @@
-<h1>aqui</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset ('/css/staly_forms.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastro de Cliente<h1>
+      <form>
+       <label> Nome do cliente 
+       <input></input>
+       </label>
+      
+       <label> Tipo:
+       <input></input>
+       <label>
+      
+       <label> Status do cliente
+       <input></input>
+       </label>
+      
+       <label> CNPJ
+       <input></input>
+       </label>
+      
+       <label> Razão social
+       <input></input>    
+       </label>
+      
+       <label> Indicador de inscrição estadual
+       <input></input>
+       </label>
+      
+       <label> Inscrição estadual
+       <input></input>
+       </label>
+       
+       <label> Inscrição municipal
+       <input></input>
+       </label>
+      
+       <label> Inscrição SUFRAMA
+       <input></input>
+       </label>
+
+       <label>  Optando pelo SIMPLES
+       <input></input>
+       </label>
+
+       <label>  Email principal 
+       <input></input>
+       </label>
+
+
+       <label>  Telefone comercial
+       <input></input>
+       </label>
+       
+       <label>  Telefone celular
+       <input></input>
+       </label>
+      
+       <label>  Data de fundação
+       <input></input>
+       </label>
+       
+       <label>  Código do cliente
+       <input></input>
+       </label>
+      
+       <label>  Observações
+       <input></input>
+       </label>
+      
+      
+       <!-- dados do cliente -->
+       
+       
+       
+       <label>
+       <input></input>
+       </label>
+       
+       <label>
+       <input></input>
+       </label>
+      
+       <label>
+       <input></input>
+       </label>
+       
+       <label>
+       <input></input>
+       </label>
+       
+       <label>
+       <input></input>
+       </label>
+       
+       <label>
+       <input></input>
+       </label>
+       
+       <label>
+       <input></input>
+       </label>
+      
+       <label>
+       <input></input>
+       </label>
+      
+       <label>
+       <input></input>
+       </label> 
+    
+      </form>
+
+</body>
+</html>
